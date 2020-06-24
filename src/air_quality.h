5b4e739a8837a1ae7e86d66f62cb02b57b716ce0
Stephen Ramsay <sramsay@protonmail.com>
2020-06-24 16:20:42 -0500

    nowa now does air quality!  Pollen counts!
    This works by finding the nearest station office, where there is much, much more data available.  I'm assuming here that individual stations do not have this data.

---
+++ src/air_quality.h
@@ -0,0 +1,9 @@
+#ifndef AIR_QUALITY_INCLUDED
+#define AIR_QUALITY_INCLUDED
+
+#include <stdbool.h>
+
+bool print_air_quality(char station_id[restrict static 1]);
+// bool print_forecast_json(char station_id[restrict static 1]);
+
+#endif
