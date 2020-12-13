75948598634176533b9bd6df3b393894afed00d6
Stephen Ramsay <sramsay@protonmail.com>
2020-12-13 14:34:19 -0600

    Modified comment

--- src/points_data.h
+++ src/points_data.h
@@ -6,7 +6,7 @@
 typedef struct points_info points_info;
 
 struct points_info {
-  char cwa[4]; // The forecast office
+  char cwa[4];  // The forecast office
   char* forecast_url;
   char* forecast_hourly_url;
   char* forecast_grid_data_url;
@@ -16,8 +16,8 @@ struct points_info {
   char* fire_weather_zone_url;
   char* radar_station;
 };
-// This JSON also includes bearing and distance (from what, exactly? the forecast office?) and
-// timezone.
+// This JSON also includes bearing and distance (from what, exactly? the
+// forecast office?) and timezone.
 
 bool init_points(char lat_long[restrict static 1], struct points_info* points);
 void cleanup_points(struct points_info* points);
